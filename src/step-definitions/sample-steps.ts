bb0df718e7c146ac4f68d7631fc6737185532ea1
Josue Ramirez <josue.rmz@gmail.com>
2024-08-19 02:46:59 -0600

    Update Github actions

--- src/step-definitions/sample-steps.ts
+++ src/step-definitions/sample-steps.ts
@@ -3,13 +3,13 @@ import {browser} from '@wdio/globals';
 
 Given('I open Google HomePage', async()=>{
     await browser.url('https://www.google.com');
-})
+});
 
 Given('I open the url {string}', async(url:string)=>{
     await browser.url(url);
-})
+});
 
 Then('The page title contains the text {string}', async(expectedPageTitle:string)=>{
     const pageTitle = await browser.getTitle();
     expect(pageTitle).toEqual(expectedPageTitle);
-})
+});
